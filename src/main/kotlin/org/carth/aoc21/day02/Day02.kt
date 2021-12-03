690389de8f884b3a4b5d6731b62ff40728935b7b
Thierry Carels <thierry.carels@gmail.com>
2021-12-03 09:12:21 +0100

    Day 02 : common

--- src/main/kotlin/org/carth/aoc21/day02/Day02.kt
+++ src/main/kotlin/org/carth/aoc21/day02/Day02.kt
@@ -3,11 +3,14 @@ package org.carth.aoc21.day02
 import org.carth.aoc21.common.Puzzle
 
 class Day02(private val data: List<String>) : Puzzle<Int, Int>() {
+
+    private fun getCommandDelta(line: String) = line.split(" ").let { it[0] to it[1].toInt() }
+
     override fun solvePartOne(): Int {
         var horizontal = 0
         var depth = 0
         data.forEach {
-            val (command, delta) = it.split(" ").let { it[0] to it[1].toInt() }
+            val (command, delta) = getCommandDelta(it)
             when (command) {
                 "forward" -> horizontal += delta
                 "down" -> depth += delta
@@ -22,7 +25,7 @@ class Day02(private val data: List<String>) : Puzzle<Int, Int>() {
         var depth = 0
         var aim = 0
         data.forEach {
-            val (command, delta) = it.split(" ").let { it[0] to it[1].toInt() }
+            val (command, delta) = getCommandDelta(it)
             when (command) {
                 "forward" -> {
                     horizontal += delta
