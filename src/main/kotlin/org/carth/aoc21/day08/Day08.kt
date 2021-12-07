7fc89492f85e0e7847f32970f494ac4b2e77857b
Thierry Carels <thierry.carels@gmail.com>
2021-12-07 13:27:47 +0100

    Day08 : prep

---
+++ src/main/kotlin/org/carth/aoc21/day08/Day08.kt
@@ -0,0 +1,13 @@
+package org.carth.aoc21.day08
+
+import org.carth.aoc21.common.Puzzle
+
+class Day08(private val data: List<String>) : Puzzle<Int, Int>() {
+    override fun solvePartOne(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun solvePartTwo(): Int {
+        TODO("Not yet implemented")
+    }
+}
---
+++ src/test/kotlin/org/carth/aoc21/day08/Day08Test.kt
@@ -0,0 +1,61 @@
+package org.carth.aoc21.day08
+
+import org.carth.aoc21.common.Resources.resourceAsListOfString
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.MethodOrderer
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Order
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestMethodOrder
+
+class Day08Tests {
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+    inner class Day08PartOneTest {
+
+        @Test
+        @Order(1)
+        fun `solve part one of sample`() {
+            // Act
+            val answer = Day08(resourceAsListOfString("day08_test.txt")).solvePartOne()
+
+            // Assert
+            assertEquals(answer, 0)
+        }
+
+        @Test
+        @Order(2)
+        fun `solve part one`() {
+            // Act
+            val answer = Day08(resourceAsListOfString("day08.txt")).solvePartOne()
+
+            // Assert
+            assertEquals(answer, 0)
+        }
+    }
+
+    @Nested
+    @TestMethodOrder(MethodOrderer.OrderAnnotation::class)
+    inner class Day08PartTwoTest {
+
+        @Test
+        @Order(1)
+        fun `solve part two of sample`() {
+            // Act
+            val answer = Day08(resourceAsListOfString("day08_test.txt")).solvePartTwo()
+
+            // Assert
+            assertEquals(answer, 0)
+        }
+
+        @Test
+        @Order(2)
+        fun `solve part two`() {
+            // Act
+            val answer = Day08(resourceAsListOfString("day08.txt")).solvePartTwo()
+            // Assert
+            assertEquals(answer, 0)
+        }
+    }
+}
