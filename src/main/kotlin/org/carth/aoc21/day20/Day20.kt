00f64e486499e0af9691d2ec85da012e051bcfb9
Thierry Carels <thierry.carels@gmail.com>
2021-12-20 17:40:54 +0100

    Day20 : faster

--- src/main/kotlin/org/carth/aoc21/day20/Day20.kt
+++ src/main/kotlin/org/carth/aoc21/day20/Day20.kt
@@ -1,73 +1,48 @@
 package org.carth.aoc21.day20
 
 import org.carth.aoc21.common.Puzzle
-import org.carth.aoc21.common.Resources
-import kotlin.system.measureTimeMillis
 
-fun main() {
-    val ms = measureTimeMillis {
-        var input = Resources.testInputAsListOfString("day20")
-        println(Day20(input).solvePartOne())
-
-        input = Resources.inputAsListOfString("day20")
-        println(Day20(input).solvePartOne())
-
-        var input2 = Resources.testInputAsListOfString("day20")
-        println(Day20(input2).solvePartTwo())
-
-        input2 = Resources.inputAsListOfString("day20")
-        println(Day20(input2).solvePartTwo())
-    }
-    println("ms : $ms")
-}
+typealias Image = Array<IntArray>
 
 class Day20(private val data: List<String>) : Puzzle<Int, Int>() {
-    override fun solvePartOne(): Int {
-        return solve(2)
-    }
 
-    override fun solvePartTwo(): Int {
-        return solve(50)
-    }
+    override fun solvePartOne(): Int = solve(2)
+
+    override fun solvePartTwo(): Int = solve(50)
 
     private fun solve(times: Int): Int {
         var (algorithm, image) = parse()
         var default = 0
-        val isInfiniteFlashing = algorithm[0] == 1
+        val isFlashing = algorithm[0] == 1
         repeat(times) {
-            val nextImage = HashMap<Pair<Int, Int>, Int>()
-            image.keys.forEach { (l, c) ->
-                for (il in -1..1) {
-                    for (ic in -1..1) {
-                        val valBin = getBinary(image, l + il, c + ic, default)
-                        nextImage[(l + il) to (c + ic)] = algorithm[valBin]
-                    }
+            val nextImage = Array(image.size + 2) { IntArray(image.size + 2) { default } }
+            for (l in nextImage.indices) {
+                for (c in nextImage.indices) {
+                    nextImage[l][c] = algorithm[getBinary(image, l - 1, c - 1, default)]
                 }
             }
-            default = if (isInfiniteFlashing && default == 0) 1 else 0
+            default = if (isFlashing && default == 0) 1 else 0
             image = nextImage
         }
-        return image.values.sum()
+        return image.sumOf { it.sum() }
     }
 
-    private fun getBinary(image: MutableMap<Pair<Int, Int>, Int>, l: Int, c: Int, default: Int): Int {
+    private fun getBinary(image: Image, l: Int, c: Int, default: Int): Int {
         var acc = 0
         for (il in -1..1) {
             for (ic in -1..1) {
-                acc = acc * 2 + image.getOrDefault((l + il to c + ic), default)
+                val value = if ((l + il) in image.indices && (c + ic) in image.indices) image[l + il][c + ic] else default
+                acc = acc * 2 + value
             }
         }
         return acc
     }
 
-    private fun parse(): Pair<Array<Int>, MutableMap<Pair<Int, Int>, Int>> {
-        val first = data.first().map { if (it == '#') 1 else 0 }.toTypedArray()
-        val grid = HashMap<Pair<Int, Int>, Int>()
-        data.drop(2).forEachIndexed { l, line ->
-            line.forEachIndexed { c, value ->
-                grid[l to c] = if (value == '#') 1 else 0
-            }
-        }
-        return first to grid
+    private fun parse(): Pair<IntArray, Image> {
+        val algorithm = data.first().map { if (it == '#') 1 else 0 }.toIntArray()
+        val image = data.drop(2).map { line ->
+            line.map { if (it == '#') 1 else 0 }.toIntArray()
+        }.toTypedArray()
+        return algorithm to image
     }
 }
